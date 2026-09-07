ddb61bf4d8b34be0a7910405521a971e80aacf02
Kate Po <hamster.k.po@gmail.com>
2026-09-07 20:35:28 +0300

    implement: the PATCH endpoint returns HTTP 422

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
 from routes import movie_router
 
@@ -9,5 +12,26 @@ app = FastAPI(
 )
 
 api_version_prefix = "/api/v1"
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+    request: Request,
+    exc: RequestValidationError,
+):
+    if (
+        request.method == "PATCH"
+        and request.url.path.startswith("/api/v1/theater/movies/")
+    ):
+        return JSONResponse(
+            status_code=400,
+            content={"detail": "Invalid input data."},
+        )
+
+    return JSONResponse(
+        status_code=422,
+        content={"detail": jsonable_encoder(exc.errors())},
+    )
+
 
 app.include_router(movie_router, prefix=f"{api_version_prefix}/theater", tags=["theater"])
